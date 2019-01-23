cadc895f3b3c95810eabfbd2fceb127da057dbe9
Carlos Gonzales <carlos.gonzales@jalasoft.com>
2019-01-23 19:36:56 -0400

    Adding create account test

--- e2e/pages/accountFormModal.po.js
+++ e2e/pages/accountFormModal.po.js
@@ -7,10 +7,19 @@ class AccountForm extends Form {
     constructor() {
         super();
         this.nameTextField = '//span[contains(text(), "Account Name")]/parent::label/following-sibling::input';
+        this.phoneTextField = '//span[contains(text(), "Phone")]/parent::label/following-sibling::input';
+        this.faxTextField = '//span[contains(text(), "Fax")]/parent::label/following-sibling::input';
     }
 
-    setName(name) {
-        commonActions.setValue(this.nameTextField, name);
+    fillForm(accountJson) {
+        let accountSteps = {
+            'Name': () => commonActions.setValue(this.nameTextField, accountJson.Name),
+            'Phone': () => commonActions.setValue(this.phoneTextField, accountJson.Phone),
+            'Fax': () => commonActions.setValue(this.faxTextField, accountJson.Fax)
+        };
+        Object.keys(accountJson).forEach(key => {
+            accountSteps[key].call();
+        });
     }
 
 }
--- e2e/specs/demo.spec.js
+++ e2e/specs/demo.spec.js
@@ -4,14 +4,23 @@ const Login = require('../pages/login.po');
 const AccountForm = require('../pages/accountFormModal.po');
 
 describe('Login to salesforce', () => {
+
     it('Login', () => {
-        let header = Login.loginAs(credentials.sysadmin.username, credentials.sysadmin.password);
+        let header = Login.loginAs(credentials.sysadmin.username,
+            credentials.sysadmin.password);
         let appLauncherModal = header.clickAppLauncherButton();
         let content = appLauncherModal.openItem('Account');
         content.clickNewButton();
 
         let accountForm = new AccountForm();
-        accountForm.setName('MyAccountName');
+        let account = {
+            'Name': 'MyAccountName',
+            'Phone': '5465465465',
+            'Fax': '5465465465',
+        };
+        accountForm.fillForm(account);
         accountForm.clickSaveButton();
+        browser.pause(30000);
+
     });
 });
